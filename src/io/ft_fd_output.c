0a3f653021b48ee269da3f957be11167bd0fdb65
seungwoo7050 <seungwoo7050@naver.com>
2025-01-15 09:35:48 +0900

    fix(io): 파일 디스크립터 출력을 끝까지 재시도

--- src/io/ft_fd_output.c
+++ src/io/ft_fd_output.c
@@ -1,28 +1,62 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include "libft.h"
 
+#include <errno.h>
+#include <limits.h>
 #include <unistd.h>
+
+static int	write_all(int fd, const char *buffer, size_t length)
+{
+	ssize_t	written;
+	size_t	offset;
+	size_t	request;
+
+	offset = 0;
+	while (offset < length)
+	{
+		request = length - offset;
+		if (request > (size_t)SSIZE_MAX)
+			request = (size_t)SSIZE_MAX;
+		written = write(fd, buffer + offset, request);
+		if (written > 0)
+			offset += (size_t)written;
+		else if (written < 0 && errno == EINTR)
+			continue ;
+		else
+		{
+			if (written == 0)
+				errno = EIO;
+			return (0);
+		}
+	}
+	return (1);
+}
 
 void	ft_putchar_fd(char character, int fd)
 {
-	(void)write(fd, &character, 1);
+	(void)write_all(fd, &character, 1);
 }
 
 void	ft_putstr_fd(char *text, int fd)
 {
-	(void)write(fd, text, ft_strlen(text));
+	(void)write_all(fd, text, ft_strlen(text));
 }
 
 void	ft_putendl_fd(char *text, int fd)
 {
-	ft_putstr_fd(text, fd);
-	ft_putchar_fd('\n', fd);
+	if (write_all(fd, text, ft_strlen(text)))
+		(void)write_all(fd, "\n", 1);
 }
 
-static void	put_unsigned(unsigned int magnitude, int fd)
+static int	put_unsigned(unsigned int magnitude, int fd)
 {
-	if (magnitude >= 10U)
-		put_unsigned(magnitude / 10U, fd);
-	ft_putchar_fd((char)('0' + magnitude % 10U), fd);
+	char	digit;
+
+	if (magnitude >= 10U && !put_unsigned(magnitude / 10U, fd))
+		return (0);
+	digit = (char)('0' + magnitude % 10U);
+	return (write_all(fd, &digit, 1));
 }
 
 void	ft_putnbr_fd(int number, int fd)
@@ -31,10 +65,11 @@ void	ft_putnbr_fd(int number, int fd)
 
 	if (number < 0)
 	{
-		ft_putchar_fd('-', fd);
+		if (!write_all(fd, "-", 1))
+			return ;
 		magnitude = (unsigned int)(-(number + 1)) + 1U;
 	}
 	else
 		magnitude = (unsigned int)number;
-	put_unsigned(magnitude, fd);
+	(void)put_unsigned(magnitude, fd);
 }
